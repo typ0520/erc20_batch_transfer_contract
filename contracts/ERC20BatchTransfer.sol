e5a932df6069a803b9b1410e5f021ced0b063c9c
typ0520 <php12345@163.com>
2020-11-09 18:44:13 +0800

    ownerTransfer

--- contracts/ERC20BatchTransfer.sol
+++ contracts/ERC20BatchTransfer.sol
@@ -2,8 +2,14 @@
 pragma solidity ^0.6.12;
 
 import 'openzeppelin-solidity/contracts/token/ERC20/IERC20.sol';
+import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-contract ERC20BatchTransfer {
+contract ERC20BatchTransfer is Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
     function transfer(IERC20 _token, address[] memory _tos, uint[] memory _values, uint _total) public returns(bool) {
         require(_tos.length == _values.length);
         uint sum = 0;
@@ -12,6 +18,19 @@ contract ERC20BatchTransfer {
             uint value = _values[i];
             sum = sum + value;
             _token.transferFrom(msg.sender, to, value);
+        }
+        require(sum == _total);
+        return true;
+    }
+
+    function ownerTransfer(IERC20 _token, address[] memory _tos, uint[] memory _values, uint _total) public onlyOwner returns(bool)  {
+        require(_tos.length == _values.length);
+        uint sum = 0;
+        for (uint i = 0; i < _tos.length; i++) {
+            address to = _tos[i];
+            uint value = _values[i];
+            sum = sum + value;
+            _token.transfer(to, value);
         }
         require(sum == _total);
         return true;
