ce1cab181b0fa8ca6eba095e47dd6d30688f7c6b
typ0520 <php12345@163.com>
2020-12-16 22:25:30 +0800

    support-eth

--- contracts/ERC20BatchTransfer.sol
+++ contracts/ERC20BatchTransfer.sol
@@ -10,6 +10,31 @@ contract ERC20BatchTransfer is Ownable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
+    function transferETH(address[] memory _tos, uint[] memory _values) public payable returns(bool) {
+        require(_tos.length == _values.length);
+        for (uint i = 0; i < _tos.length; i++) {
+            address payable to = address(uint160(_tos[i]));
+            uint value = _values[i];
+            to.transfer(value);
+        }
+        return true;
+    }
+
+    // function transferETH(address[] memory _tos, uint[] memory _values, uint _total) public payable returns(bool) {
+    //     require(_tos.length == _values.length);
+    //     require(msg.value >= _total);
+    //     uint sum = 0;
+    //     for (uint i = 0; i < _tos.length; i++) {
+    //         address payable to = address(uint160(_tos[i]));
+
+    //         uint value = _values[i];
+    //         sum = sum + value;
+    //         to.transfer(value);
+    //     }
+    //     require(sum == _total);
+    //     return true;
+    // }
+
     function transfer(IERC20 _token, address[] memory _tos, uint[] memory _values, uint _total) public returns(bool) {
         require(_tos.length == _values.length);
         uint sum = 0;
